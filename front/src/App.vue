301e31f71574d2f680c59d08e9794a2f4f7a4272
yaominc <kenops1a@qq.com>
2021-12-30 17:20:19 +0800

    测试栅格布局

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <v-app id="inspire">
+  <!-- 主项目 -->
+  <!--<v-app id="inspire">
     <Header/>
     <router-view></router-view>
     <Footer/>
-  </v-app>
+  </v-app>-->
+
+  <!-- 栅格布局测试 -->
+  <!--<div>
+    <GridTest></GridTest>
+  </div>-->
+
+  <!-- 登录测试 -->
+  <LoginTest></LoginTest>
 </template>
 <script>
 
 export default {
   name: 'App',
   components: {
-    Header: () => import('@/components/Header/Header'),
-    Footer: () => import('@/components/Footer/Footer')
+    // Header: () => import('@/components/Header/Header'),
+    // Footer: () => import('@/components/Footer/Footer'),
+    // GridTest: () => import('@/components/Test/GridTest')
+    LoginTest: () => import('@/components/Test/LoginTest')
   },
 
   data () {
---
+++ front/src/components/Test/LoginTest.vue
@@ -0,0 +1,19 @@
+<!--
+* @Descripttion:
+* @Project: outage
+* @Author: yaominc
+* @Date: 2021/12/30
+-->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "LoginTest"
+}
+</script>
+
+<style scoped>
+
+</style>
